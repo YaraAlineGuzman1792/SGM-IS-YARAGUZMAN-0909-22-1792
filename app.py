461f5ef405ce7f3be57f5e75e2526f86f78beefd
Aline <yaraalineguzman@gmail.com>
2026-06-15 14:22:31 -0600

    Se corrige app.py}

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from datetime import datetime
+import os
 import uuid
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
+app = Flask(__name__, template_folder=template_dir)
 
 app = Flask(__name__)
 app.secret_key = "sgm-municipalidad-guatemala-2026"
